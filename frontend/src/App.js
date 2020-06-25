e212a9d3879f6078bec7bee37107b205edcbb659
yashraj2312 <yashrajsrivastava2312@gmail.com>
2020-06-25 18:16:11 +0530

    adding controllers and some modifications

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Container } from 'reactstrap'
+import Routes from './routes';
+import { ContextWrapper } from './user-context'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ContextWrapper>
+      <Container>
+        <h1>Sports Event App</h1>
+        <div className="content">
+          <Routes />
+        </div>
+      </Container>
+    </ContextWrapper>
   );
 }
 
